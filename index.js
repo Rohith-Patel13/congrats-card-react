4e8476bd0805d7a8f140f4ab03effbe60a3ef3e8
Rohith Patel <rohithgithub999@gmail.com>
2023-08-15 20:56:27 +0530

    react code of congrats card

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const element = (
     // Write your code here.
+    <div className="bg">
+    <h1 className="mainHeading">Congratulations</h1>
+    <div className="profileCard">
+      <img
+        src="-tjwj8l.jpg"
+        className="profilePic"
+      />
+      <h1 className="cardHead">Rohith Patel</h1>
+      <p className="cardPara">CMR Technical Campus,Hyderabad</p>
+      <p></p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="watchImage"
+      />
+    </div>
+  </div>
   )
   
 ReactDOM.render(element, document.getElementById('root'))
